3deb7f1cd0dbd2ad60baf6ab0d29eed7d459c932
Hafune <hafu985@gmail.com>
2024-08-23 18:49:00 +0500

    added joystick in example

--- Assets/Core/Test/PlayerController.cs
+++ Assets/Core/Test/PlayerController.cs
@@ -9,16 +9,21 @@ namespace Core.Test
         [SerializeField] private Rigidbody _body;
         [SerializeField] private float _speed;
         private PlayerInputs.PlayerActions _playerInputs;
+        private FixedJoystick _joystick;
 
         private void Start()
         {
             Context.Resolve<CinemachineVirtualCamera>().Follow = transform;
             _playerInputs = Context.Resolve<PlayerInputs.PlayerActions>();
+            _joystick = Context.Resolve<FixedJoystick>();
         }
 
         private void FixedUpdate()
         {
             var direction = _playerInputs.Move.ReadValue<Vector2>();
+
+            if (direction == Vector2.zero)
+                direction = _joystick.Direction;
             
             _body.velocity = new Vector3(direction.x, 0, direction.y) * _speed;
         }
